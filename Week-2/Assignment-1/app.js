ba97fbaccf46df1956037b5affec128f7f0a1593
Jackie Chen <jackie@Jackies-MacBook-Air.local>
2023-04-03 21:38:54 +0800

    assignment 1

---
+++ Week-2/Assignment-1/app.js
@@ -0,0 +1,27 @@
+function max(numbers) {
+// your code here, for-loop method preferred
+    let max = 0;
+    for(let i=0; i<numbers.length; i++){
+        max = (numbers[i] > max)? numbers[i]:max;
+    }
+    return max;
+}
+function findPosition(numbers, target) {
+// your code here, for-loop method preferred
+    for(let i=0; i<numbers.length; i++){
+        if(numbers[i] === target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+console.log(max([1, 2, 4, 5])); // should print 5
+console.log(max([5, 2, 7, 1, 6])); //should print 6
+
+console.log(findPosition([5, 2, 7, 1, 6], 5)) // 0
+console.log(findPosition([5, 2, 7, 1, 6], 7)) // 2
+console.log(findPosition([5, 2, 7, 7, 7, 1, 6], 7))// 2
+console.log(findPosition([5, 2, 7, 1, 6], 8)) // -1
+
