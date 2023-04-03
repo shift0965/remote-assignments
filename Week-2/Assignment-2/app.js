cc03bdfb650f0ec4ea5f93ff11e916aa2da64c50
Jackie Chen <jackie@Jackies-MacBook-Air.local>
2023-04-03 21:48:15 +0800

    assignment-2

---
+++ Week-2/Assignment-2/app.js
@@ -0,0 +1,26 @@
+function avg (data){
+    // your code here
+    return data.products.reduce((total, product) => total+product.price, 0)/data.size
+}
+
+
+
+console.log (
+avg ({
+    size: 3,
+    products:[
+        {
+            name: 'Product 1',
+            price: 100,
+        },
+        {
+            name: 'Product 2', 
+            price: 700,
+        },
+        {
+            name: 'Product 3', 
+            price: 250,
+        }
+        ]
+    })
+) // should print the average price of all products
