e1b7daa715a4e566d6026389ce250c3971e1ec5b
Jackie Chen <jackie@Jackies-MacBook-Air.local>
2023-04-18 08:35:51 +0800

    create the html, css and js files for assignment1
    Add html and css files from week-3 and write delayResult with callback functions. Add a listener at submit button and call delayResult and render the result in headline when countdown is out

---
+++ Week-4/Assignments/public/js/assignment1.js
@@ -0,0 +1,52 @@
+function delayedResult(n1, n2, delayTime, callback) {
+  /*
+  setTimeout(function () {
+    callback(n1 + n2);
+  }, delayTime);
+  */
+  let leftTime = delayTime;
+  callback(leftTime / 1000);
+  setInterval(function () {
+    leftTime -= 1000;
+    if (leftTime <= 0) {
+      const result = n1 + n2;
+      callback("The result is <br>" + result);
+    } else {
+      callback(leftTime / 1000);
+    }
+  }, 1000);
+}
+
+/*
+delayedResult(4, 5, 3000, function (result) {
+  console.log(result);
+}); // 9 (4+5) will be shown in the console after 3 seconds
+
+delayedResult(-5, 10, 2000, function (result) {
+  console.log(result);
+}); // 5 (-5+10) will be shown in the console after 2 seconds
+*/
+
+const formSubmit = document.querySelector("#formSubmit");
+const number1 = document.querySelector("#number1");
+const number2 = document.querySelector("#number2");
+const delayTime = document.querySelector("#delayTime");
+const headline = document.querySelector("#headline");
+
+formSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+  const n1 = parseInt(number1.value);
+  const n2 = parseInt(number2.value);
+  const delay = parseInt(delayTime.value) * 1000;
+  if (isNaN(n1) || isNaN(n2) || isNaN(delay)) {
+    render("Please enter valid numbers");
+  } else {
+    delayedResult(n1, n2, delay, render);
+  }
+});
+
+function render(result) {
+  headline.innerHTML = `
+  <h1>${result}</h1>
+  `;
+}
