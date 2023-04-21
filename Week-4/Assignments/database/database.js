b5ee30610c5512b9838bd3c1699f9464e4fa4555
Jackie Chen <jackie@Jackies-MacBook-Air.local>
2023-04-21 20:41:35 +0800

    add an errorhandle function and change id between to limit offset in getArticlesByUserAndId

--- Week-4/Assignments/database/database.js
+++ Week-4/Assignments/database/database.js
@@ -19,38 +19,42 @@ export async function connectdb() {
   }
 }
 
-//user tbale
-
-export async function getUsers() {
+async function handleError(callback) {
   try {
     const db = await connectdb();
-    const [rows, field] = await db.execute("SELECT * FROM user;");
-    const users = rows;
-    return users;
+    const result = await callback(db);
+    return result;
   } catch (err) {
-    console.error(err);
-    return err;
+    console.log(err);
+    return { Error: err.code };
   }
 }
 
+//user tbale
+
+export async function getUsers() {
+  const result = await handleError(async (db) => {
+    const [rows, field] = await db.execute("SELECT * FROM user;");
+    const users = rows;
+    return users;
+  });
+  return result;
+}
+
 export async function checkEmailExist(email) {
-  try {
-    const db = await connectdb();
+  const result = await handleError(async (db) => {
     const [rows, field] = await db.execute(
       "SELECT id FROM user WHERE email =?",
       [email]
     );
     if (rows.length > 0) return { exist: true };
     else return { exist: false };
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+  });
+  return result;
 }
 
 export async function checkEmailPassword(email, password) {
-  try {
-    const db = await connectdb();
+  const result = await handleError(async (db) => {
     const [rows, field] = await db.execute(
       "SELECT id, username FROM user WHERE email =? AND password =?",
       [email, password]
@@ -58,55 +62,44 @@ export async function checkEmailPassword(email, password) {
     const user = rows[0];
     if (user) return { user };
     else return { user: null };
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+  });
+  return result;
 }
 
 export async function addUser(username, email, password) {
-  try {
-    const db = await connectdb();
+  const result = await handleError(async (db) => {
     const [rows, field] = await db.execute(
       "INSERT INTO user (username, email, password) VALUES (?,?,?);",
       [username, email, password]
     );
     const id = rows.insertId;
     return { user: { id, username } };
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+  });
+  return result;
 }
 
 export async function removeUser(id) {
-  try {
-    const db = await connectdb();
+  const result = await handleError(async (db) => {
     const [rows, field] = await db.execute("DELETE FROM user WHERE id=?", [id]);
     if (rows.affectedRows === 1) {
       return { userRemoved: true };
     } else {
       return { userRemoved: false };
     }
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+  });
+  return result;
 }
 
 //articles table
 
 export async function getArticles() {
-  try {
-    const db = await connectdb();
+  const result = await handleError(async (db) => {
     const [rows, field] = await db.execute(
       "SELECT articles.id, username, email, content FROM articles LEFT JOIN user ON articles.author_email = user.email ORDER BY username;"
     );
     return rows;
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+  });
+  return result;
 }
 
 export async function getArticlesByUserAndId(
@@ -114,19 +107,16 @@ export async function getArticlesByUserAndId(
   lower = 0,
   upper = 99
 ) {
-  try {
-    const db = await connectdb();
+  const result = await handleError(async (db) => {
     const [rows, field] = await db.execute(
       `SELECT articles.id, username, email, content FROM 
         articles LEFT JOIN user ON articles.author_email = user.email 
-        WHERE user.username LIKE ? AND
-        articles.id BETWEEN ? AND ?
-        ORDER BY username, articles.id;`,
-      ["%" + username + "%", lower, upper]
+        WHERE user.username LIKE ?
+        ORDER BY articles.id
+        LIMIT ? OFFSET ?;`,
+      ["%" + username + "%", upper - lower, lower]
     );
     return rows;
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+  });
+  return result;
 }
